c20f182d71eea0c1189b6786e9a90fe85d797139
MerinJ1997 <100820871+MerinJ1997@users.noreply.github.com>
2022-03-03 16:22:31 +0530

    Add project files.

---
+++ PharmacyManagement/Commands/AddUserCommand.cs
@@ -0,0 +1,49 @@
+﻿using PharmacyManagement.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PharmacyManagement.Commands
+{
+    public class AddUserCommand : ICommand
+    {
+        public MainModelView model;
+        public AddUserCommand(MainModelView viewModel)
+        {
+            this.model = viewModel;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "addUser")
+
+            {
+                 model.SelectedView = new AddUserViewModel();
+            }
+            else if (parameter.ToString() == "ViewUpdateUser")
+
+            {
+                model.SelectedView = new ViewUpdateUserViewModel();
+            }
+            else if (parameter.ToString() == "ViewStock")
+            {
+                model.SelectedView = new StockViewModel();
+            }
+            else if (parameter.ToString() == "AddStock")
+            {
+                model.SelectedView = new AddStockViewModel();
+            }
+           
+
+        }
+    }
+}
---
+++ PharmacyManagement/ViewModel/MainModelView.cs
@@ -0,0 +1,29 @@
+﻿using PharmacyManagement.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PharmacyManagement.ViewModel
+{
+   public class MainModelView:BaseViewModel
+    {
+        private BaseViewModel _selectedview = new HomeViewModel();
+
+        public BaseViewModel SelectedView
+        {
+            get { return _selectedview; }
+            set { _selectedview = value; OnPropertyChanged(nameof(SelectedView)); }
+        }
+        public ICommand AddUserCommand { get; set; }
+
+        public MainModelView()
+        {
+            AddUserCommand = new AddUserCommand(this);
+        }
+
+
+    }
+}

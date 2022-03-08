5c876c12a07e7642333a5d5db1dbce22759d8e5a
SheemaPK <100774569+SheemaPK@users.noreply.github.com>
2022-03-08 22:32:21 +0530

    Search Button

--- PharmacyManagement/View/SearchView.xaml.cs
+++ PharmacyManagement/View/SearchView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,45 @@ namespace PharmacyManagement.View
     /// </summary>
     public partial class SearchView : UserControl
     {
+        SqlConnection  con= null;
+        SqlDataAdapter adap = null;
         public SearchView()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                MessageBox.Show("Searching for result");
+                con = new SqlConnection("data source =.; database = PharmacyManagement; integrated security = SSPI");
+                con.Open();
+                string search = TextBox1.Text;
+                string cmdst = "select MedicineName,Price,StockAvailable from StockDetails where MedicineName like '"+search+"%'";
+                
+                adap = new SqlDataAdapter(cmdst, con);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+                con.Close();
+                GridView.ItemsSource = (System.Collections.IEnumerable)ds;
+                //for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                //{
+                //    string first = ds.Tables[0].Rows[i]["MedicineName"].ToString();
+                //    string second=ds.Tables[1].Rows[i]["Price"].ToString();
+                //    string Third = ds.Tables[2].Rows[i]["StockAvailable"].ToString();
+
+
+                //}
+                
+                //GridView1.da= ds.Tables[0];
+                //GridView1.DataBind();
+            }
+            catch (Exception pp)
+            {
+                Console.WriteLine("No data found");
+            }
+        } 
     }
 }

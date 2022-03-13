50fedb722528823eacab7f0861f277ca58924ad7
MerinJ1997 <100820871+MerinJ1997@users.noreply.github.com>
2022-03-13 22:45:48 +0530

    Validation Done

--- PharmacyManagement/Commands/StockSubmitCommand.cs
+++ PharmacyManagement/Commands/StockSubmitCommand.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace PharmacyManagement.Commands
@@ -18,6 +20,8 @@ namespace PharmacyManagement.Commands
         {
             addStockViewModel = ViewModel;
         }
+        Regex rName = new Regex(@"^[a-zA-Z]+$");
+        Regex rUnit = new Regex(@"^[0-9]+$");
 
         public event EventHandler CanExecuteChanged;
 
@@ -28,16 +32,51 @@ namespace PharmacyManagement.Commands
 
         public void Execute(object parameter)
         {
-
+            string Name = addStockViewModel.MedName;
+            string Company = addStockViewModel.Company;
+            string UnitPrice = addStockViewModel.Price.ToString();
+            string StockAvailable = addStockViewModel.Stocks.ToString();
+            string Expiry = addStockViewModel.Expiry.ToString();
             StockModel sm = new StockModel();
             UpdateStockBusiness updateStockBusiness = new UpdateStockBusiness();
-            sm.MedID = addStockViewModel.MedId;
-            sm.MedName = addStockViewModel.MedName;
-            sm.Company=addStockViewModel.Company;
-            sm.UnitPrice = addStockViewModel.Price;
-            sm.StockAvailable = addStockViewModel.Stocks;
-            sm.Expiry = addStockViewModel.Expiry;
-            updateStockBusiness.SaveStockBussiness(sm);
+
+            if (Name != null && Company != null && UnitPrice != null && StockAvailable != null && Expiry != null)
+            {
+                if(Name.Length > 0)
+                {
+                    if (!rName.IsMatch(Name))
+                    {
+                        MessageBox.Show("Invalid Medicine Name");
+                    }
+                    if (!rName.IsMatch(Company))
+                    {
+                        MessageBox.Show("Invalid Medicine Name");
+                    }
+                    else if (!rUnit.IsMatch(UnitPrice))
+                    {
+                        MessageBox.Show("Invalid Price");
+                    }
+                    else if (!rUnit.IsMatch(StockAvailable))
+                    {
+                        MessageBox.Show("Invalid Price");
+                    }
+                    else
+                    {                        
+                        sm.MedID = addStockViewModel.MedId;
+                        sm.MedName = addStockViewModel.MedName;
+                        sm.Company = addStockViewModel.Company;
+                        sm.UnitPrice = addStockViewModel.Price;
+                        sm.StockAvailable = addStockViewModel.Stocks;
+                        sm.Expiry = addStockViewModel.Expiry;
+                        updateStockBusiness.SaveStockBussiness(sm);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter Values");
+            }
+            
     
         }
     }
